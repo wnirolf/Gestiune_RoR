544e2c18e0fc9e791b30767b17f3a52c7fb73a4d
wnirolf <wnirolf@hotmail.com>
2013-01-08 09:11:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestiuneRor::Application.config.secret_token = 'ac969f457bf9f7aae1344d3b40c1673d17ddb0aa5de19b21fd63022508687b44035555fa23d8f6190074b09834ddf645e0e2c2eee2a4d471cc6487c34ddb02a6'
